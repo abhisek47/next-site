746a30ae9ebc28cd24f1e39cf4929ff3a7e97127
Abhisek Paul <abhisekpaul@Abhiseks-MacBook-Air.local>
2020-10-08 14:17:56 +0530

    remove sitemap

--- next.config.js
+++ next.config.js
@@ -28,13 +28,3 @@ module.exports = withWorkbox({
 
 const withImages = require('next-images');
 module.exports = withImages({});
-
-module.exports = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require('./scripts/generate-sitemap');
-    }
-
-    return config;
-  },
-};
