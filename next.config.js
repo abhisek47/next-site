602bb2f10ffe2e4007d780bc1a2700a94211bca8
Abhisek Paul <abhisekpaul@Abhiseks-MacBook-Air.local>
2020-10-08 14:14:23 +0530

    add sitemap and robots.txt

--- next.config.js
+++ next.config.js
@@ -28,3 +28,13 @@ module.exports = withWorkbox({
 
 const withImages = require('next-images');
 module.exports = withImages({});
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./scripts/generate-sitemap');
+    }
+
+    return config;
+  },
+};
